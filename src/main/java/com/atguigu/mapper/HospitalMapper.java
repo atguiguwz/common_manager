3b30cf2824ac571a9944c9968875599361c6642e
atguigu <wangze@atguigu.com>
2022-06-14 13:28:49 +0800

    master

--- src/main/java/com/atguigu/mapper/HospitalMapper.java
+++ src/main/java/com/atguigu/mapper/HospitalMapper.java
@@ -4,7 +4,7 @@ import com.atguigu.entity.Hospital;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Service;
 
-//test 111
+//test 111 qqq
 @Service
 public interface HospitalMapper extends BaseMapper<Hospital> {
 }
