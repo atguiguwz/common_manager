b1d4b0f153abbd8985b3a973eb81d6545a571db4
atguigu <wangze@atguigu.com>
2022-06-14 13:44:50 +0800

    xiugai1

--- src/main/java/com/atguigu/mapper/HospitalMapper.java
+++ src/main/java/com/atguigu/mapper/HospitalMapper.java
@@ -5,7 +5,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Service;
 
 //test 111 qqq
-///
+/// test
 @Service
 public interface HospitalMapper extends BaseMapper<Hospital> {
 }
